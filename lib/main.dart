711164b36d092ea7397221f2bf443463c20f0390
Le An <anlv.it.vn@gmail.com>
2019-11-20 14:23:15 +0700

    Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,30 @@
+import 'package:database_sqlflite/utils/database_helper.dart';
 import 'package:flutter/material.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:path_provider/path_provider.dart';
 
-
+import 'models/user.dart';
 
 void main() async {
+  List _users;
+  var db = new DatabaseHelper();
+
+  //update user
+  User anUpdated = User.fromMap({
+    "username": "anUpdated",
+    "password": "passwordUpdated",
+    "id": 1,
+  });
+
+  int re = await db.updateUser(anUpdated);
+  print(re);
+
+  _users = await db.getAllUsers();
+
+  for (int i = 0; i < _users.length; i++) {
+    User u = User.map(_users[i]);
+    print(
+        'Username : ${u.username} and Password : ${u.password} and ID: ${u.id}');
+  }
+
   runApp(new MaterialApp(
     title: "Database",
     home: HomePage(),
@@ -23,5 +43,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
-
--- lib/utils/database_helper.dart
+++ lib/utils/database_helper.dart
@@ -1,25 +1,28 @@
+import 'dart:core';
 import 'dart:io';
 
+import 'package:database_sqlflite/models/user.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
-class DatabaseHelper{
+
+class DatabaseHelper {
   static final DatabaseHelper _instance = new DatabaseHelper.internal();
 
   factory DatabaseHelper() => _instance;
 
   static Database _db;
 
-  final String tableUser ="userTable";
-  final String columnId  = "id";
+  final String tableUser = "userTable";
+  final String columnId = "id";
   final String columnUsername = "username";
   final String columnPassword = "password";
 
-  Future<Database> get db async{
-    if (_db !=null){
+  Future<Database> get db async {
+    if (_db != null) {
       return _db;
     }
-    
+
     _db = await initDb();
     return _db;
   }
@@ -30,13 +33,63 @@ class DatabaseHelper{
     Directory documentDirectory = await getApplicationDocumentsDirectory();
     String path = join(documentDirectory.path, 'maindb.db');
 
-    var ourDB = await openDatabase(path,version: 1,onCreate: _onCreate);
-
+    var ourDB = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return ourDB;
   }
-  _onCreate(Database db, int version) async{
+
+  _onCreate(Database db, int version) async {
     await db.execute(
-        "CREATE TABLE $tableUser($columnId PRIMARY KEY AUTOINCREMENT, $columnUsername TEXT, $columnPassword TEXT)");
+        "CREATE TABLE $tableUser($columnId INTEGER PRIMARY KEY AUTOINCREMENT, $columnUsername TEXT, $columnPassword TEXT)");
   }
 
+  //CRUD: CREATE, READ, UPDATE, DELETE
 
+  //CRUD : 1-CREATE
+  Future<int> saveUser(User user) async {
+    var dbClient = await db;
+    int res = await dbClient.insert("$tableUser", user.toMap());
+    return res;
+  }
+
+  Future<int> getCountUser() async {
+    var dbClient = await db;
+    return Sqflite.firstIntValue(
+        await dbClient.rawQuery("SELECT COUNT(*) FROM $tableUser"));
+  }
+
+  //CRUD : 2-READ
+  Future<List> getAllUsers() async {
+    var dbClient = await db;
+    var result = await dbClient.rawQuery("SELECT * FROM $tableUser");
+    return result.toList();
+  }
+
+  Future<User> getUser(int id) async {
+    var dbClient = await db;
+    var result = await dbClient
+        .rawQuery("SELECT * FROM $tableUser WHERE $columnId = $id");
+    if (result.length == 0) {
+      return null;
+    }
+    return new User.fromMap(result.first);
+  }
+
+  //CRUD : 4-DELETE
+
+  Future<int> deleteUser(int id) async {
+    var dbClient = await db;
+    return await dbClient
+        .delete("$tableUser", where: "$columnId = ?", whereArgs: [id]);
+  }
+
+  Future<int> updateUser(User user) async {
+    var dbClient = await db;
+    return await dbClient.update('$tableUser', user.toMap(),
+        where: "$columnId = ?", whereArgs: [user.id]);
+  }
+
+  Future close() async {
+    var dbClient = await db;
+    return dbClient.close();
+  }
 }
